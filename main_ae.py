b77bad9452ab503b23dac420f6fe3302dba95673
varunvarmat <varunvarma2002@gmail.com>
2025-03-20 14:39:23 +0530

    initial build

---
+++ main_ae.py
@@ -0,0 +1,33 @@
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import math
+# import copy
+import numpy as np
+import json
+from torch.utils.data import random_split, TensorDataset
+from utils import train_test_loader, format_data
+# to do:
+# 1.copy data(properties) to this machine 
+# 2.change properties.json to required format
+# 3.build train and test loaders
+# 4.design model architecture /data/varun/CrossFlow/libs/model/trans_autoencoder.py
+
+           
+
+
+
+        
+
+    
+
+def main():
+    data=format_data()
+    trainloader,testloader=train_test_loader(data)
+
+
+
+
+
+if __name__=='__main__':
+    main()
